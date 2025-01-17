c82ce1186262f97d23e929e001530356853d9c8a
test-rahu <rahul.rathod@hurix.com>
2025-01-17 18:56:04 +0530

    pull from dipali

--- src/test/java/com/Epicon/TestCases/testclassForGit.java
+++ src/test/java/com/Epicon/TestCases/testclassForGit.java
@@ -4,7 +4,7 @@ public class testclassForGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Welcome");
+System.out.println("Welcome dipali");
 	}
 
 }
