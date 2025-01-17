0cd4ddb334e0c9bfca0b33c65c7a72980a29ccc5
test-rahu <rahul.rathod@hurix.com>
2025-01-17 18:48:36 +0530

    Newclasscommit

---
+++ src/test/java/com/Epicon/TestCases/testclassForGit.java
@@ -0,0 +1,10 @@
+package com.Epicon.TestCases;
+
+public class testclassForGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome");
+	}
+
+}
